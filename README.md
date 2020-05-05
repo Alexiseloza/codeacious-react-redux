718e02d57b7c1e5af8def1c308ef570cdfcfb0f7
Alexis Edgar Loza <47640237+Alexiseloza@users.noreply.github.com>
2020-05-05 20:35:24 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# E-commerce Shopping Cart Application
+Single-page e-commerce shopping cart
+Implemented with React, Redux, react-redux hooks (useSelector, useDispatch) & native hook (useEffect)
+
+# Credit
+Codeacious project. Powered by Alexis Loza
+
+# Translations
+Necesitas una App de React en español ??
---
+++ src/components/Copyright.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+export default class Copyright extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Powered By Alexis,   New React Developer Codeacious</h4>
+        <ul>
+          <li>
+             Implemented Redux
+          </li>
+          <li>
+            Implemented react-redux hooks (useSelector, useDispatch) and useEffects hook
+          </li>
+          <li>
+            <a href="https://github.com/Alexiseloza/React-Hooks-Redux-Codeacious">
+              Source code
+            </a>
+          </li>    
+          <li>
+            <a href="https://github.com/Alexiseloza">
+            for the Codeacious Ninjas
+            </a>
+          </li>              
+        </ul>
+      </div>
+    );
+  }
+}
